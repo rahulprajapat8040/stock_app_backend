e34bde6369d752785055bb22b380feec1e8664fe
rahulprajapat8040 <rahulkp7272@gmail.com>
2025-09-10 23:09:09 +0530

    update

--- src/app/admin/add/page.tsx
+++ src/app/admin/add/page.tsx
@@ -1,11 +1,6 @@
 // page.tsx (Server Component)
-import { Suspense } from "react";
 import AddForm from "./AddForm";
 
 export default function Page() {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <AddForm />
-        </Suspense>
-    );
+    return <AddForm />; // Just render it directly
 }
