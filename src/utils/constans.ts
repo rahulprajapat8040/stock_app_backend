35e4e5ad4802b582279f87f2a11bed5be9ce69e5
rahulprajapat8040 <rahulkp7272@gmail.com>
2025-09-10 21:29:20 +0530

    updated

--- src/utils/constans.ts
+++ src/utils/constans.ts
@@ -1,2 +1,2 @@
-// export const Base_Url = `https://api.playgolddwin.com`
-export const Base_Url = `http://localhost:4505`
+export const Base_Url = `https://api.playgolddwin.com`
+// export const Base_Url = `http://localhost:4505`
