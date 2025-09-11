a0abe1be724d0c2dc07dcaf6ad2aeb865ef141a9
rahulprajapat8040 <rahulkp7272@gmail.com>
2025-09-11 11:26:12 +0530

    updated

--- src/app/results-sheet/page.tsx
+++ src/app/results-sheet/page.tsx
@@ -81,8 +81,8 @@ export default async function Home({
                                                 </td>
                                                 <td className="
   border border-gray-400 px-1 text-left
-  text-[11px]              /* base <400px */
-  [@media(min-width:400px)]:text-[13px]   /* 400px+ */
+  text-[11.5px]              /* base <400px */
+  [@media(min-width:410px)]:text-[13px]   /* 400px+ */
   sm:text-[16px]           /* 640px+ */
   md:text-[14px]           /* 768px+ */
 ">
